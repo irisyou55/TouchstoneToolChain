09734be231b7d28307eab1114ac9b2a691efd6c1
ylht <wqs.97@icloud.com>
2020-08-18 20:36:06 +0800

    [feature]: Reservoir sampling Algorithm L

--- src/main/java/ecnu/db/generation/JoinInfoTable.java
+++ src/main/java/ecnu/db/generation/JoinInfoTable.java
@@ -7,9 +7,10 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static java.lang.Math.*;
@@ -26,15 +27,20 @@ public class JoinInfoTable implements Externalizable {
     /**
      * join info table，map status -> key list
      */
-    private ConcurrentMap<Long, List<int[]>> joinInfo = new ConcurrentHashMap<>();
+    private Map<Long, List<int[]>> joinInfo = new HashMap<>();
 
-    private final ConcurrentMap<Long, Long> counters = new ConcurrentHashMap<>();
+    /**
+     * 记录Reservoir sampling Algorithm L 中的状态数据结构，对于每个status list记录三个状态值，按照顺序为
+     * 1. 当前list被add的次数n
+     * 2. 当前list下一个允许被add的item的index
+     * 3. 随机概率值W
+     * 使用double记录次数可以保证，在 2^52≈4.5E15 范围内的整数的次数可以被准确的记录，而数据生成的任务量，一般不会达到这个量级
+     */
+    private final Map<Long, double[]> counters = new HashMap<>();
 
-    private final ConcurrentMap<Long, Double> paramWs = new ConcurrentHashMap<>();
 
-    private final ConcurrentMap<Long, Long> nextIdxs = new ConcurrentHashMap<>();
-
-    public JoinInfoTable() {}
+    public JoinInfoTable() {
+    }
 
     public static void setMaxListSize(int maxListSize) {
         JoinInfoTable.maxListSize = maxListSize;
@@ -69,6 +75,7 @@ public class JoinInfoTable implements Externalizable {
 
     /**
      * 根据join status获取符合条件的主键值
+     *
      * @param status join status
      * @return 所有复合主键
      */
@@ -81,76 +88,35 @@ public class JoinInfoTable implements Externalizable {
      * reservoir sampling following Algorithm L in "Reservoir-Sampling Algorithms of Time Complexity O(n(1+log(N/n)))"
      *
      * @param status join status
-     * @param key 一个复合主键
+     * @param key    一个复合主键
      */
     public void addJoinInfo(long status, int[] key) {
-        counters.compute(status, (s1, counts) -> {
-           if (counts == null) {
-               counts = 0L;
-           }
-           if (counts < maxListSize) {
-               joinInfo.compute(status, (s2, keys) -> {
-                   if (keys == null) {
-                       keys = new ArrayList<>(maxListSize);
-                   }
-                   keys.add(key);
-                   return keys;
-               });
-           }
-           else {
-               double paramW = paramWs.compute(status, (s2, w) -> {
-                   if (w == null) {
-                       w = exp(logRandom() / maxListSize);
-                   }
-                   return w;
-               });
-               double finalWeight = paramW;
-               long nextIdx = nextIdxs.compute(status, (s3, idx) -> {
-                    if (idx == null) {
-                        idx = generateNextIdx(finalWeight, maxListSize);
-                    }
-                    return idx;
-               });
-               if (counts == nextIdx) {
-                   joinInfo.computeIfPresent(status, (s2, keys) -> {
-                       int j = ThreadLocalRandom.current().nextInt(0, maxListSize);
-                       keys.set(j, key);
-                       return keys;
-                   });
-                   nextIdx = generateNextIdx(paramW, nextIdx);
-                   paramW = paramW * exp(logRandom() / maxListSize);
-                   paramWs.put(status, paramW);
-                   nextIdxs.put(status, nextIdx);
-               }
-           }
-           counts++;
-           return counts;
-        });
+        //如果不存在该status，初始化计数器，predict id和权重w
+        double[] counter = counters.computeIfAbsent(status, k -> {
+                    double w = exp(log(-ThreadLocalRandom.current().nextDouble(-1, 0)) / maxListSize);
+                    return new double[]{0, maxListSize + predictIdOffset(w), w};
+                }
+        );
+        counter[0]++;
+        if (counter[0] < maxListSize) {
+            joinInfo.computeIfAbsent(status, k -> new ArrayList<>(maxListSize)).add(key);
+        } else {
+            if (counter[0] >= counter[1]) {
+                counter[1] += predictIdOffset(counter[2]);
+                joinInfo.get(status).set(ThreadLocalRandom.current().nextInt(maxListSize), key);
+                counter[2] *= exp(log(-ThreadLocalRandom.current().nextDouble(-1, 0)) / maxListSize);
+            }
+        }
     }
 
     /**
-     * 用于计算下一个可以插入的idx
-     * @param weight W参数
-     * @param previousIdx 前一个可以插入的idx
-     * @return 下一个可以插入的idx
+     * 计算下一个可以被加入的index
+     *
+     * @param w 权重weight
+     * @return 下一个可以被加入的index
      */
-    private static long generateNextIdx(double weight, long previousIdx) {
-        return previousIdx + ((long) (floor(logRandom() / log(1 - weight)))) + 1;
-    }
-
-    /**
-     * 计算一个log(random()), 出于防止overflow的目的, 设置了一个下界
-     * @return log(random())
-     */
-    private static double logRandom() {
-        return log(ThreadLocalRandom.current().nextDouble(1e-7, 1));
-    }
-
-    /**
-     * 清除key list中用于压缩计数的tag
-     */
-    public void cleanKeyCounter() {
-        counters.clear();
+    private double predictIdOffset(double w) {
+        return floor(log(-ThreadLocalRandom.current().nextDouble(-1, 0)) / log(1 - w)) + 1;
     }
 
     @Override
